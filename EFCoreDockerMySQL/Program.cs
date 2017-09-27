6a1f99b4f090873cbef2b55b56612cbc2ca5d0e6
Andrea Vallotti <andrea.vallotti@gmail.com>
2017-09-27 23:12:24 +0200

    Read and write people from MySQL

--- EFCoreDockerMySQL/Program.cs
+++ EFCoreDockerMySQL/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace EFCoreDockerMySQL
 {
@@ -6,7 +7,45 @@ namespace EFCoreDockerMySQL
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            builder.UseMySql("server=efcoredockermysql-mysql;userid=root;pwd=p4ssw0r#;port=3306;database=efcoredockermysql;sslmode=none;");
+            AddOrUpdateDavid(builder.Options);
+            ShowAllPeople(builder.Options);
+        }
+
+        private static void AddOrUpdateDavid(DbContextOptions<ApplicationDbContext> options)
+        {
+            using (var context = new ApplicationDbContext(options))
+            {
+                var david = context.People.FirstOrDefaultAsync(x => x.Name == "David").Result;
+
+                if (david != null)
+                {
+                    david.Surname += "*";
+                }
+                else
+                {
+                    david = new Person
+                    {
+                        Name = "David",
+                        Surname = "Gilmour"
+                    };
+                    context.People.Add(david);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        private static void ShowAllPeople(DbContextOptions<ApplicationDbContext> options)
+        {
+            using (var context = new ApplicationDbContext(options))
+            {
+                foreach (var person in context.People)
+                {
+                    Console.WriteLine($"{person.Name} {person.Surname}");
+                }
+            }
         }
     }
 }

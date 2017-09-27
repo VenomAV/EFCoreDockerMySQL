e885c153e736ce676dfabcf69732d3cbe59c96d5
Andrea Vallotti <andrea.vallotti@gmail.com>
2017-09-27 08:59:31 +0200

    Add data model and DbContext

---
+++ EFCoreDockerMySQL/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreDockerMySQL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : 
+            base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
---
+++ EFCoreDockerMySQL/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCoreDockerMySQL
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+    }
+}

8fdd41240f4159cff226fbc26312d65bfd8cd9a1
Andrea Vallotti <andrea.vallotti@gmail.com>
2017-10-03 22:53:38 +0200

    Add email migration

--- EFCoreDockerMySQL/Migrations/20171003205013_AddEmail.Designer.cs
+++ EFCoreDockerMySQL/Migrations/20171003205013_AddEmail.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace EFCoreDockerMySQL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171003205013_AddEmail")]
+    partial class AddEmail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EFCoreDockerMySQL/Migrations/20171003205013_AddEmail.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace EFCoreDockerMySQL.Migrations
+{
+    public partial class AddEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "People",
+                type: "longtext",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "People");
+        }
+    }
+}

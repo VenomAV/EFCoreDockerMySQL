fa25bf82570c7f6bf17f3c7110219987589a8f0f
Andrea Vallotti <andrea.vallotti@gmail.com>
2017-10-03 22:40:35 +0200

    Add implementation of IDesignTimeDbContextFactory<ApplicationDbContext> in order to build DbContext for migrations

---
+++ EFCoreDockerMySQL/ApplicationDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCoreDockerMySQL
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            return new ApplicationDbContext(new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseMySql(Program.Configuration.GetConnectionString("DefaultConnection"))
+                .Options);
+        }
+    }
+}
